5e01d253598b93034bb7b7de8adb29883650bd7b
OlegGeivandov <9327337@mail.ru>
2023-09-29 19:47:03 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x=0;
+        Set<String> set = new TreeSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("¬ведите слово");
+
+            String str = scanner.nextLine();
+            if (set.contains(str)) {
+                System.out.println("уже было");
+                x++;
+            }
+            else {
+                set.add(str);
+
+            }
+
+        }
+        while (x<3);
+        System.out.println(set);
+
+
     }
 }
